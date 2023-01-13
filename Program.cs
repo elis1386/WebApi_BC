a339d870b3b77c1cc92149939f64532891a28530
elis1386 <s2200704@BC-C02J64A8Q6LR-MacBook-Air.local>
2023-01-13 14:00:55 +0200

    add mysql db todo1

--- Program.cs
+++ Program.cs
@@ -2,10 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
+// builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseMySql("server=localhost;database=todo1;user=root", serverVersion));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
